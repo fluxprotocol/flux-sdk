a75be31fbed8aa66e39c6db4504bfac7631f91f2
Jasper De gooijer <jasperdegooijer@gmail.com>
2020-03-12 15:27:46 +0100

    reworked the way a walletconnection is established by replacing the WalletAccount with WalletConnection logic, also created a new fake connection to make all unit tests work

--- tests/test_utils.js
+++ tests/test_utils.js
@@ -5,9 +5,39 @@ const nearlib = require('nearlib');
 
 const networkId = 'unittest';
 const testAccountName = 'test.near';
+const keyStore = new nearlib.keyStores.BrowserLocalStorageKeyStore();
 
 const INITIAL_BALANCE = new BN(100000000000);
 const HELLO_WASM_PATH = './tests/flux_protocol.wasm';
+
+async function setUpTestFluxConnection(workingAccount, contractId) {
+    await keyStore.setKey(networkId, contractId, nearlib.utils.KeyPair.fromString('ed25519:2wyRcSwSuHtRVmkMCGjPwnzZmQLeXLzLLyED1NDMt4BjnKgQL6tF85yBx6Jr26D2dUNeC716RBoTxntVHsegogYw'));
+	const config = Object.assign(require('./config')(process.env.NODE_ENV || 'test'), {
+		networkId: networkId,
+		deps: { keyStore },
+	});
+	let keyPair = nearlib.KeyPair.fromRandom('ed25519');
+	
+    
+    const near = await nearlib.connect(config);
+    const walletConnection = new nearlib.WalletConnection(near, contractId);
+    walletConnection._authData = {
+        allKeys: [ 'no_such_access_key', keyPair.publicKey.toString() ],
+        accountId: workingAccount.accountId
+    };
+
+    const contract = new nearlib.Contract(workingAccount, contractId, {
+        viewMethods: ["get_all_markets", "get_fdai_balance", "get_market", "get_market_order", "get_owner", "get_claimable", "get_open_orders", "get_filled_orders", "get_fdai_metrics"],
+        changeMethods: ["create_market", "claim_fdai" ,"delete_market", "place_order", "claim_earnings", "resolute_market"],
+        sender: walletConnection.getAccountId(),
+    });
+
+    return {
+        near,
+        walletConnection,
+        contract
+    }
+}
 
 async function setUpTestConnection() {
     const keyStore = new nearlib.keyStores.InMemoryKeyStore();
@@ -16,7 +46,6 @@ async function setUpTestConnection() {
         networkId: networkId,
         deps: { keyStore },
     });
-
     return nearlib.connect(config);
 }
 
@@ -53,5 +82,5 @@ async function ensureDir(dirpath) {
     }
 }
 
-module.exports = { setUpTestConnection, networkId, testAccountName, INITIAL_BALANCE,
+module.exports = { setUpTestFluxConnection, setUpTestConnection, networkId, testAccountName, INITIAL_BALANCE,
     generateUniqueString, createAccount, deployContract, sleep, ensureDir };
