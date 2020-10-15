dc2fcbd432d727a8f4164032a56becb90210368c
jasperdg <jasperdegooijer@gmail.com>
2020-10-15 17:59:52 +0200

    finished implementing typescript, all previous tests passing.

--- src/tests/chain.test.ts
+++ src/tests/chain.test.ts
@@ -1,11 +1,11 @@
 import FluxProvider from '../index';
-import { Account, Contract, keyStores, Near } from 'near-api-js';
-import { Market } from '../types';
+import { Account, keyStores, Near } from 'near-api-js';
 import {
     setUpTestConnection,
     generateUniqueString,
     createAccount,
-    deployContracts
+    deployContracts,
+    toDai
 } from "./testUtils";
 
 let near: Near;
@@ -60,74 +60,74 @@ describe('chain tests', () => {
     });
 
 
-    // it("Is able to create a market", async () => {
-    //     const endTime = (new Date().getTime() + 70000 ).toString();
-    //     await flux.createBinaryMarket("This is a test binary market", "", [] , endTime, "1", "5", "");
-    //     await flux.createCategoricalMarket("This is a test categorical market", "", "3", ["yes", "no", "maybe"],[], endTime, "1", "5", "");
-    // });
+    it("Is able to create a market", async () => {
+        const endTime = new Date().getTime() + 70000 ;
+        await flux.createBinaryMarket("This is a test binary market", "", [] , endTime, 1, 5);
+        await flux.createCategoricalMarket("This is a test categorical market", "", 3, ["yes", "no", "maybe"],[], endTime, 1);
+    });
 
-    // it("Is able to place a limit order", async () => {
-    //     await flux.placeOrder("0", "0", "50000", "10", flux.getAccountId());
-    //     await flux.placeOrder("1", "0", "50000", "10", flux.getAccountId());
-    // });
+    it("Is able to place a limit order", async () => {
+        await flux.placeOrder(0, 0, "50000", 10);
+        await flux.placeOrder(1, 0, "50000", 10);
+    });
 
-    // it("Is able to cancel an order", async () => {
-    //     await flux.cancelOrder("0", "0", "0", "10");
-    //     await flux.cancelOrder("1", "0", "0", "10");
-    // });
+    it("Is able to cancel an order", async () => {
+        await flux.cancelOrder(0, 0, 0, 10);
+        await flux.cancelOrder(1, 0, 0, 10);
+    });
 
-    // it("Is able to fill a limit order", async () => {
-    //     await flux.placeOrder("0","0", "10000", "50", flux.getAccountId());
-    //     await flux.placeOrder("1", "1", "10000", "50", flux.getAccountId());
-    // });
+    it("Is able to fill a limit order", async () => {
+        await flux.placeOrder(0,0, "10000", 50);
+        await flux.placeOrder(1, 1, "10000", 50);
+    });
 
-    // it("Is able to fill a market order", async () => {
-    //     await flux.placeOrder("0", "0", "10000", "50");
-    //     await flux.placeOrder("1", "0", "10000", "50");
-    //     await flux.placeOrder("0", "1", "10000", "50");
-    //     await flux.placeOrder("1", "1", "10000", "50");
-    // });
+    it("Is able to fill a market order", async () => {
+        await flux.placeOrder(0, 0, "10000", 50);
+        await flux.placeOrder(1, 0, "10000", 50);
+        await flux.placeOrder(0, 1, "10000", 50);
+        await flux.placeOrder(1, 1, "10000", 50);
+    });
 
-    // it("Is able to dynamically sell to the market", async () => {
-    //     const endTime = (new Date().getTime() + 20000 ).toString();
-    //     await flux.createBinaryMarket("This is a test binary market", "", [], endTime, "1", "5", "");
-    //     await flux.placeOrder("2", "0", "20000", "50");
-    //     await flux.placeOrder("2", "0", "20000", "50");
-    //     await flux.placeOrder("2", "1", "20000", "50");
-    //     const shareBalance = await flux.getShareBalanceForUserForMarket("2", flux.account.accountId);
-    //     await flux.dynamicMarketSell("2", "0", "50", "50")
-    // });
+    it("Is able to dynamically sell to the market", async () => {
+        const endTime = new Date().getTime() + 20000;
+        await flux.createBinaryMarket("This is a test binary market", "", [], endTime, 1, 5);
+        await flux.placeOrder(2, 0, "20000", 50);
+        await flux.placeOrder(2, 0, "20000", 50);
+        await flux.placeOrder(2, 1, "20000", 50);
+        const shareBalance = await flux.getShareBalanceForUserForMarket(2, flux.account!.accountId);
+        await flux.dynamicMarketSell(2, 0, "50", 50)
+    });
 
-    // it("Is able to resolute a market", async () => {
-    //     await flux.resolute("0", "0", toDai(5).toString());
-    //     await flux.resolute("1", "0", toDai(5).toString());
-    //     const binaryClaimable = await flux.getClaimable("0");
-    //     const categoricalClaimable = await flux.getClaimable("1");
-    //     expect(Object.keys(binaryClaimable)).not.toBe(0);
-    //     expect(Object.keys(categoricalClaimable)).not.toBe(0);
-    // });
+    it("Is able to resolute a market", async () => {
+        await flux.resolute(0, 0, toDai(5).toString());
+        await flux.resolute(1, null, toDai(5).toString());
+        const binaryClaimable = await flux.getClaimable(0);
+        const categoricalClaimable = await flux.getClaimable(1);
+        expect(Object.keys(binaryClaimable)).not.toBe(0);
+        expect(Object.keys(categoricalClaimable)).not.toBe(0);
+    });
 
-    // it("Is able to withdraw dispute on a market", async() => {
-    //     await flux.dispute("1", "1", toDai(9).toString());
-    //     await flux.withdrawDisputeStake("1", "1", "1");
-    // });
+    it("Is able to withdraw dispute on a market", async() => {
+        await flux.dispute(1, 1, toDai(9).toString());
+        await flux.withdrawDisputeStake(1, 1, 1);
+    });
 
-    // it("Is able to dispute a market", async () => {
-    //     await flux.dispute("0", "1", toDai(10).toString());
-    //     await flux.dispute("1", "1", toDai(10).toString());
-    // });
+    it("Is able to dispute a market", async () => {
+        await flux.dispute(0, 1, toDai(10).toString());
+        await flux.dispute(1, 1, toDai(10).toString());
+    });
 
-    // it("Is able to finalize a market", async () => {
-    //     await flux.finalize("0", "0");
-    //     await flux.finalize("1", "1");
-    // });
+    it("Is able to finalize a market", async () => {
+        await flux.finalize(0, 0);
+        await flux.finalize(1, 1);
+    });
 
-    // it("Is able to claim earnings", async () => {
-    //     const balance = await flux.getBalance(workingAccount.accountId);
-    //     await flux.claimEarnings("0", flux.getAccountId());
-    //     const updatedBalance = await flux.getBalance(workingAccount.accountId);
-    //     expect(parseInt(updatedBalance) > parseInt(balance));
-    // });
+    it("Is able to claim earnings", async () => {
+        const balance = await flux.getBalance(workingAccount.accountId);
+        await flux.claimEarnings(0, flux.getAccountId()!);
+        const updatedBalance = await flux.getBalance(workingAccount.accountId);
+        expect(parseInt(updatedBalance) > parseInt(balance));
+    });
 
     
 });
