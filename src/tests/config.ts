9ee5ab5eb61b80222458892a6ab5307544b29af0
jasperdg <jasperdegooijer@gmail.com>
2020-10-20 10:51:20 +0200

    increased coverage

--- src/tests/config.ts
+++ src/tests/config.ts
@@ -1,13 +1,6 @@
 export default function getConfig(env: string) {
 	switch (env) {
 	case 'production':
-	case 'local':
-			return {
-					networkId: 'local',
-					nodeUrl: 'http://localhost:3030',
-					keyPath: `${process.env.HOME}/.near/validator_key.json`,
-					walletUrl: 'http://localhost:4000/wallet',
-			};
 	case 'test':
 	case 'ci':
 			return {
--- src/tests/indexer.test.ts
+++ src/tests/indexer.test.ts
@@ -37,146 +37,61 @@ describe("indexer", () => {
     const res: Array<any> = await flux.getLastFilledPrices({}, 10, 0);
   })
   
-  it("can get specific market ", async() => {
+  it("can get specific market ", async () => {
     const res = await flux.getMarket(2);
   })
   
-  it("can get market prices for specific market ", async() => {
+  it("can get market prices for specific market ", async () => {
     const res = await flux.getMarketPrices(2);
   })
   
-  it("can get average praces of a specific date", async() => {
+  it("can get average praces of a specific date", async () => {
     const res = await flux.getAvgPricesOnDate(2, "2020-07-17");
   })
   
-  it("can get a users open orders for a specific market", async() => {
+  it("can get a users open orders for a specific market", async () => {
     const res = await flux.getOpenOrdersForUserForMarket(2, "flux-dev");
   })
   
-  it("can get a user's outcome balance for a specific market", async() => {
+  it("can get a user's outcome balance for a specific market", async () => {
     const res = await flux.getShareBalanceForUserForMarket(2, "flux-dev");
   })
   
-  it("can get historic chart data", async() => {
+  it("can get historic chart data", async () => {
     const res = await flux.getPriceHistory(0, "2020-07-20", "2020-07-28", ["day"]);
   })
   
-  it("can get orderbook", async() => {
+  it("can get orderbook", async () => {
     const res = await flux.getOrderbook(2);
   })
   
-  it("can get a user's affiliate earnings", async() => {
+  it("can get a user's affiliate earnings", async () => {
     const res = await flux.getAffiliateEarnings("flux-dev");
   })
   
-  it("can get a user's open orders", async() => {
+  it("can get a user's open orders", async () => {
     const res = await flux.getOpenOrders("flux-dev");
   })
   
-  it("can get a user's order history", async() => {
+  it("can get a user's order history", async () => {
     const res = await flux.getOrderHistory("flux-dev");
   })
   
-  it("can get all finalized markets that a user participated in", async() => {
+  it("can get all finalized markets that a user participated in", async () => {
     const res = await flux.getOrderHistory("flux-dev");
     expect(res.length == 0);
   })
   
-  it("can all markets that are currently resoluting", async() => {
+  it("can all markets that are currently resoluting", async () => {
     const res = await flux.getOrderHistory("flux-dev");
     expect(res.length == 3);
   })
   
-  it("can get resolution state", async() => {
+  it("can get resolution state", async () => {
     const res = await flux.getResolutionState({}, 0, 1);
   })
   
-  it("can get trade earnings", async() => {
-    const res = await flux.getTradeEarnings(0, "flux-dev");
-  })
-})
-
-
-describe("indexer", () => {
-  it("can get active markets from index node", async () => {
-    const markets: Array<Market> = await flux.getMarkets({}, 20, 0);
-    
-    const sportsMarkets: Array<Market> = await flux.getMarkets({categories: ["sports"]}, 1, 0);
-    expect(sportsMarkets.length == 1);
-    
-    const firstMarket: Array<Market> = await flux.getMarkets({}, 1, 0);
-    expect(firstMarket.length == 1);
-  })
-  
-  it("can get all resoluting markets from index node", async () => {
-    const markets: Array<Market> = await flux.getResolutingMarkets({}, 20, 0);
-    
-    const sportsMarkets: Array<Market> = await flux.getResolutingMarkets({categories: ["sports"]}, 1, 0);
-    expect(sportsMarkets.length == 1);
-    
-    const firstMarket: Array<Market> = await flux.getResolutingMarkets({}, 1, 0);
-    expect(firstMarket.length == 1);
-  })
-  
-  it("test lastFilledPrices", async () => {
-    const res: Array<any> = await flux.getLastFilledPrices({}, 10, 0);
-  })
-  
-  it("can get specific market ", async() => {
-    const res = await flux.getMarket(2);
-  })
-  
-  it("can get market prices for specific market ", async() => {
-    const res = await flux.getMarketPrices(2);
-  })
-  
-  it("can get average praces of a specific date", async() => {
-    const res = await flux.getAvgPricesOnDate(2, "2020-07-17");
-  })
-  
-  it("can get a users open orders for a specific market", async() => {
-    const res = await flux.getOpenOrdersForUserForMarket(2, "flux-dev");
-  })
-  
-  it("can get a user's outcome balance for a specific market", async() => {
-    const res = await flux.getShareBalanceForUserForMarket(2, "flux-dev");
-  })
-  
-  it("can get historic chart data", async() => {
-    const res = await flux.getPriceHistory(0, "2020-07-20", "2020-07-28", ["day"]);
-  })
-  
-  it("can get orderbook", async() => {
-    const res = await flux.getOrderbook(2);
-  })
-  
-  it("can get a user's affiliate earnings", async() => {
-    const res = await flux.getAffiliateEarnings("flux-dev");
-  })
-  
-  it("can get a user's open orders", async() => {
-    const res = await flux.getOpenOrders("flux-dev");
-  })
-  
-  it("can get a user's order history", async() => {
-    const res = await flux.getOrderHistory("flux-dev");
-  })
-  
-  it("can get all finalized markets that a user participated in", async() => {
-    const res = await flux.getOrderHistory("flux-dev");
-    expect(res.length == 0);
-  })
-  
-  it("can all markets that are currently resoluting", async() => {
-    const res = await flux.getOrderHistory("flux-dev");
-    expect(res.length == 3);
-  })
-  
-  it("can get resolution state", async() => {
-    const res = await flux.getResolutionState({}, 0, 1);
-  })
-  
-  it("can get trade earnings", async() => {
+  it("can get trade earnings", async () => {
     const res = await flux.getTradeEarnings(0, "flux-dev");
   })
 })
